b85fd7403ad65bd115f7d0080e3179c0c6e8f6ec
Renji <lprenji@gmail.com>
2021-06-20 17:21:54 +0200

    added metadata for token

--- nodeLogin/src/consts.js
+++ nodeLogin/src/consts.js
@@ -2,6 +2,6 @@ const PORT = process.env.PORT || 3000;
 const HASURA_HOST = process.env.HASURA_HOST || "localhost";
 const HASURA_PORT = process.env.HASURA_PORT || 8080;
 const HASURA_SECRET = process.env.HASURA_SECRET || "";
-const JWT_SECRET = process.env.JWT_SECRET || "HASURA_GRAPHQL_JWT_SECRET";
+const JWT_SECRET = process.env.JWT_SECRET || "HASURA_GRAPHQL_JWT_SECRET_WITH_AT_LEAST_32_CHARS";
 
 module.exports = { PORT, HASURA_HOST, HASURA_PORT, HASURA_SECRET, JWT_SECRET }
